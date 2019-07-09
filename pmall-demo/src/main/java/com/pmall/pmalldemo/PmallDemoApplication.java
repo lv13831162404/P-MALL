e787b0e2875fd65ec47ce049f6090bba83d92395
吕宏生 <635916412@qq.com>
2019-07-09 23:13:47 +0800

    创建实现类

---
+++ pmall-demo/src/main/java/com/pmall/pmalldemo/PmallDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pmall.pmalldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmallDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmallDemoApplication.class, args);
+    }
+
+}
---
+++ pmall-demo/src/main/java/com/pmall/pmalldemo/service/impl/pmall.java
@@ -0,0 +1,8 @@
+package com.pmall.pmalldemo.service.impl;
+
+public class pmall {
+
+    public static void main(String[] args) {
+        System.out.println("111111");
+    }
+}
