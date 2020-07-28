4365c350cbf8d765601020a6b1b7ce69e874d5ab
lv13831162404 <49578419+lv13831162404@users.noreply.github.com>
2020-07-28 20:14:34 +0800

    Update pmall.java

--- pmall-demo/src/main/java/com/pmall/pmalldemo/service/impl/pmall.java
+++ pmall-demo/src/main/java/com/pmall/pmalldemo/service/impl/pmall.java
@@ -5,6 +5,5 @@ public class pmall {
     public static void main(String[] args) {
         System.out.println("111111");
         System.out.println("222222");
-        System.out.println("222222");
     }
 }
